090521a326b93af24711d278aa74177d707f05c2
hrishikeshkarekar <78920259+hrishikeshkarekar@users.noreply.github.com>
2021-02-13 00:59:11 +0530

    Update views.py

--- Application/python_webapp_flask/views.py
+++ Application/python_webapp_flask/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from python_webapp_flask import app
 
 import requests
