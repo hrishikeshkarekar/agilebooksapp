adbebeb202958734beb73a957ba37a1c8bcbec9d
hrishikeshkarekar <78920259+hrishikeshkarekar@users.noreply.github.com>
2021-02-13 00:15:00 +0530

    Update views.py

--- Application/python_webapp_flask/views.py
+++ Application/python_webapp_flask/views.py
@@ -16,6 +16,10 @@ def home():
         year=datetime.now().year,
     )
 
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
