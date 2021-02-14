ee23fdbc197e1670794be4ddccf9c9b573df5552
hrishikeshkarekar <hrishikesh.karekar@outlook.com>
2021-02-14 14:26:53 +0530

    2nd functional test after local fixes

--- Tests/functional_tests/test_functional_searchgivesresults.py
+++ Tests/functional_tests/test_functional_searchgivesresults.py
@@ -6,48 +6,51 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-
 import unittest
 import os
 import sys
 import pytest
 import time
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 class FunctionalTests_SearchGivesResults(unittest.TestCase):
 
-	def setUp(self):
-		options = webdriver.ChromeOptions()
-		options.add_argument('--no-sandbox')
-		self.driver = webdriver.Chrome(chrome_options=options)
-		self.driver.implicitly_wait(300)
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.implicitly_wait(300)
 
-	def test_selenium(self):
-		webAppUrl = pytest.config.getoption('webAppUrl')
-		start_timestamp = time.time()
-		end_timestamp = start_timestamp + 60*10
-		while True:
-			try:
-				response = self.driver.get(webAppUrl)
-				element = self.driver.find_element_by_id("searchterm")
-				element.send_keys("kanban")
-				# self.driver.implicitly_wait(20)
-				element = self.driver.find_element_by_id("searchnowbtn")
-				element.click()
+    def test_selenium(self):
+        webAppUrl = pytest.config.getoption('webAppUrl')
+        start_timestamp = time.time()
+        end_timestamp = start_timestamp + 60*10
+        while True:
+            try:
+                response = self.driver.get(webAppUrl)
+                element = self.driver.find_element_by_id("searchterm")
+                element.send_keys("kanban")
 
-				element = WebDriverWait(self.driver, 10).until(
-					EC.visibility_of_element_located((By.ID, "searchnowbtn"))
-				)
-				self.assertNotEqual(element.text, "", "Value returned is bad : ")
+                element = self.driver.find_element_by_id("searchnowbtn")
+                element.click()
 
-			except Exception as e:
-				print('"##vso[task.logissue type=error;]Test test_selenium failed with error: ' + str(e))
-				current_timestamp = time.time()
-				if(current_timestamp > end_timestamp):
-					raise
-				time.sleep(5)
+                element = WebDriverWait(self.driver, 10).until(
+                        EC.visibility_of_element_located((By.ID, "searchnowbtn"))
+                )
+                self.assertNotEqual(element.text, "", "Value returned is bad : ")
 
-	def tearDown(self):
-		try:
-			self.driver.quit()
-		except Exception as e:
-			print('tearDown.Error occurred while trying to close the selenium chrome driver: ' + str(e))
+                break
+
+            except Exception as e:
+                print('"##vso[task.logissue type=error;]Test test_selenium failed with error: ' + str(e))
+                current_timestamp = time.time()
+                if(current_timestamp > end_timestamp):
+                    raise
+                time.sleep(5)
+
+    def tearDown(self):
+        try:
+            self.driver.quit()
+        except Exception as e:
+            print('tearDown.Error occurred while trying to close the selenium chrome driver: ' + str(e))
